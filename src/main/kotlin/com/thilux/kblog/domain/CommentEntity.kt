ccb406b62822d8af5c1b4bd9afc4d37dc1c52bd4
thilux <thilux.systems@gmail.com>
2018-01-09 14:40:49 -0300

    Post and Comment entities table definition.

---
+++ src/main/kotlin/com/thilux/kblog/domain/CommentEntity.kt
@@ -0,0 +1,14 @@
+package com.thilux.kblog.domain
+
+import org.jetbrains.squash.definition.*
+
+/**
+ * Created by tsantana on 05/01/18.
+ */
+
+object CommentEntity : TableDefinition() {
+    val id = integer("id").autoIncrement().uniqueIndex()
+    val commentator = varchar("commentator", 100)
+    val content = blob("content")
+    val post = reference(PostEntity.id, "post_id")
+}
---
+++ src/main/kotlin/com/thilux/kblog/domain/PostEntity.kt
@@ -0,0 +1,14 @@
+package com.thilux.kblog.domain
+
+import org.jetbrains.squash.definition.*
+
+/**
+ * Created by tsantana on 05/01/18.
+ */
+
+object PostEntity : TableDefinition() {
+    val id = integer("id").autoIncrement()
+    val title = varchar("title", 256)
+    val content = blob("content")
+    val creationDate = datetime("creation_date")
+}
